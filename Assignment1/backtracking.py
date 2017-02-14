8a3fbed0b2d68977c46e7022e0c69a50ce3c934a
kram1138 <kram1138@hotmail.com>
2017-02-13 22:30:54 -0600

    Constraining

--- Assignment1/backtracking.py
+++ Assignment1/backtracking.py
@@ -1,24 +1,25 @@
 from makePuzzles import *
+import copy
 
 wallNums = ['0','1','2','3','4']
 
 def checkEdgeCorner(puz,i,j):
-    # not an edge or corner status = 0
-    # an edge, status = 1
-    # a corner status = 2
-    status = 0
-    if(i==0 or i==len(puz)-1):
-        status+=1
-    if(j==0 or j==len(puz[0])-1):
-        status+=1
+	# not an edge or corner status = 0
+	# an edge, status = 1
+	# a corner status = 2
+	status = 0
+	if(i==0 or i==len(puz)-1):
+		status+=1
+	if(j==0 or j==len(puz[0])-1):
+		status+=1
 
-    return status
+	return status
 
 def countWalls(puz,i,j):
 	#counts the number of walls adjacent to a cell
 	#count increases 1 for walls 0-2.
 	#Increases 2 for walls 3-4
-	count = 0;
+	count = 0
 	if i>0 and puz[i-1][j] in wallNums:
 		count+=int(int(puz[i-1][j])/2+1)
 	if i<len(puz)-1 and puz[i+1][j] in wallNums:
@@ -31,7 +32,7 @@ def countWalls(puz,i,j):
 
 def checkAdjBeams(puz,i,j):
 	# counts the number of beams that are adjacent to the point
-	count = 0;
+	count = 0
 	if i>0 and puz[i-1][j]=='+':
 		count+=1
 	if i<len(puz)-1 and puz[i+1][j]=='+':
@@ -43,8 +44,29 @@ def checkAdjBeams(puz,i,j):
 	return count
 
 def findConstraining(puz,notassigned):
-	print('underconstruction')
-	quit()
+	rows = len(puz)
+	cols = len(puz[0])
+	winners = []
+	maxCount = 0
+	
+	# marks the rays
+	raytraceAll(puz)
+	for val in notassigned:
+		i = val[0]
+		j = val[1]
+		count = raytraceOne(puz, i, j)
+		
+		if count > maxCount:
+			winners = [val]
+			maxCount = count
+		elif count == maxCount:
+			winners.append(val)
+	
+	deRaytrace(puz)
+	
+	#print(winners)
+			
+	return winners[random.randint(0,len(winners)-1)]
 
 def findConstrained(puz,notassigned):
 	rows = len(puz)
@@ -52,34 +74,12 @@ def findConstrained(puz,notassigned):
 	winner = (-1,-1)
 
 	# marks the rays
-	for i in range(rows):
-		for j in range(cols):
-
-
-			#"raytrace" away from bulbs, setting all _ to + in a line in every direction
-			if puz[i][j] == "b":
-				k = 1
-				while i + k < rows and (puz[i + k][j] == "_" or puz[i + k][j] == "+"):
-					puz[i + k][j] = "+"
-					k += 1
-				k = 1
-				while i - k >= 0 and (puz[i - k][j] == "_" or puz[i - k][j] == "+"):
-					puz[i - k][j] = "+"
-					k += 1
-				k = 1
-				while j + k < cols and (puz[i][j + k] == "_" or puz[i][j + k] == "+"):
-					puz[i][j + k] = "+"
-					k += 1
-				k = 1
-				while j - k >= 0 and (puz[i][j - k] == "_" or puz[i][j - k] == "+"):
-					puz[i][j - k] = "+"
-					k += 1
-				k = 1
+	raytraceAll(puz)
 
 	# for each unassigned, check the contrained priority
 	for val in notassigned:
-		i = int(val/rows)
-		j = int(val%rows)
+		i = val[0]
+		j = val[1]
 
 		# check to see how many adjacent walls
 		walls = countWalls(puz,i,j)
@@ -97,28 +97,21 @@ def findConstrained(puz,notassigned):
 		if curr > winner[0]:
 			winner=(curr,val)
 
-	#revert all '+' to '_'
-	for i in range(rows):
-		for j in range(cols):
-			if puz[i][j] == "+":
-				puz[i][j] = "_"
+	deRaytrace(puz)
 
 	return winner[1]
 
-
-
-
 def getFromHeuristic(puz,notassigned,type):
 	if type == 'random':
-	    index = random.randint(0,len(notassigned)-1)
-	    ret = notassigned[index]
-	    notassigned.remove(ret)
+		index = random.randint(0,len(notassigned)-1)
+		ret = notassigned[index]
+		notassigned.remove(ret)
 
 	if type == 'constrained':
 		ret = findConstrained(puz,notassigned)
 		notassigned.remove(ret)
 
-	if type == 'contraining':
+	if type == 'constraining':
 		ret = findConstraining(puz,notassigned)
 		notassigned.remove(ret)
 
@@ -131,21 +124,40 @@ def printPuzzle(puz):
 		for j in range(0, cols):
 			print(puz[i][j], end=" ")
 		print("\n")
-
-def complete(puz):
-	global wallNums
+		
+def raytraceOne(puz, i, j):
 	rows = len(puz)
 	cols = len(puz[0])
-	retVal = True
+	count = 0
+	#"raytrace" away from bulb, setting all _ to + in a line in every direction
+	k = 1
+	while i + k < rows and (puz[i + k][j] == "_" or puz[i + k][j] == "+"):
+		if puz[i + k][j] == "_":
+			count += 1
+		k += 1
+	k = 1
+	while i - k >= 0 and (puz[i - k][j] == "_" or puz[i - k][j] == "+"):
+		if puz[i - k][j] == "_":
+			count += 1
+		k += 1
+	k = 1
+	while j + k < cols and (puz[i][j + k] == "_" or puz[i][j + k] == "+"):
+		if puz[i][j + k] == "_":
+			count += 1
+		k += 1
+	k = 1
+	while j - k >= 0 and (puz[i][j - k] == "_" or puz[i][j - k] == "+"):
+		if puz[i][j - k] == "_":
+			count += 1
+		k += 1
+
+	return count
+				
+def raytraceAll(puz):
+	rows = len(puz)
+	cols = len(puz[0])
 	for i in range(rows):
 		for j in range(cols):
-			if puz[i][j] in wallNums and int(puz[i][j])!=numAdjacent(puz,i,j):
-				return False
-
-	for i in range(rows):
-		for j in range(cols):
-
-
 			#"raytrace" away from bulbs, setting all _ to + in a line in every direction
 			if puz[i][j] == "b":
 				k = 1
@@ -166,18 +178,36 @@ def complete(puz):
 					k += 1
 				k = 1
 
-
-	#print("==== Check completeness")
-	#printPuzzle(puz)
+def deRaytrace(puz):
+	rows = len(puz)
+	cols = len(puz[0])
+	retVal = True
 	for i in range(rows):
 		for j in range(cols):
 			if puz[i][j] == "_":
 				retVal = False
 			elif puz[i][j] == "+":
 				puz[i][j] = "_"
-	#if retVal == False:
-		#print("Not all lit")
+
 	return retVal
+	
+def complete(puz):
+	global wallNums
+	rows = len(puz)
+	cols = len(puz[0])
+
+	for i in range(rows):
+		for j in range(cols):
+			if puz[i][j] in wallNums and int(puz[i][j]) != numAdjacent(puz,i,j):
+				return False
+
+	raytraceAll(puz)
+
+	if debug == "some":
+		print("==== Check completeness")
+		printPuzzle(puz)
+		
+	return deRaytrace(puz)
 
 def numAdjacent(puz, i, j):
 	rows = len(puz)
@@ -193,56 +223,48 @@ def numAdjacent(puz, i, j):
 		count += 1
 	return count
 
+def insidePuzzle(puz, i, j):
+	rows = len(puz)
+	cols = len(puz[0])
+	return i >=0 and j >=0 and i < rows and j < cols
+	
 def legal(puz,i,j):
 	rows = len(puz)
 	cols = len(puz[0])
-	#print("==== Check legality")
-	#printPuzzle(puz)
-	k=i-1
-	if k>=0 and puz[k][j] in wallNums:
-		if numAdjacent(puz,k,j) > int(puz[k][j]):
-			return False
-	while k>=0 and not puz[k][j] in wallNums:
-		if puz[k][j] == 'b':
-			return False
-		k-=1
+	
+	if debug == "some":
+		print("==== Check legality")
+		printPuzzle(puz)
+	
+	dx = (-1, 1, 0, 0)
+	dy = (0, 0, -1, 1)
+	for count in range(4):
+		k = i + dx[count]
+		l = j + dy[count]
+		
+		#check if we placed the bulb beside
+		if insidePuzzle(puz, k, l) and puz[k][l] in wallNums:
+			if numAdjacent(puz, k, l) > int(puz[k][l]):
+				return False
+				
+		while insidePuzzle(puz, k, l) and not puz[k][l] in wallNums:
+			if puz[k][l] == 'b':
+				return False
+			k += dx[count]
+			l += dy[count]
 
-	k=i+1
-	if k<rows and puz[k][j] in wallNums:
-		if numAdjacent(puz,k,j) > int(puz[k][j]):
-			return False
-	while k<rows and not puz[k][j] in wallNums:
-		if puz[k][j] == 'b':
-			return False
-		k+=1
-
-	k=j-1
-	if k>=0 and puz[i][k] in wallNums:
-		if numAdjacent(puz,i,k) > int(puz[i][k]):
-			return False
-	while k>=0 and not puz[i][k] in wallNums:
-		if puz[i][k] == 'b':
-			return False
-		k-=1
-
-	k=j+1
-	if k<cols and puz[i][k] in wallNums:
-		if numAdjacent(puz,i,k) > int(puz[i][k]):
-			return False
-	while k<cols and not puz[i][k] in wallNums:
-		if puz[i][k] == 'b':
-			return False
-		k+=1
+	if debug == "some":
+		print("Legal")
 	return True
 
 def getBlanks(puz):
-	retVal = []
-	for i in range(0,len(puz)*len(puz[0])):
-		k = int(i/len(puz))
-		j = int(i%len(puz[0]))
-		if puz[k][j] == "_":
-			retVal.append(i)
-	return retVal
+	blanks = []
+	for k in range(0,len(puz)*len(puz[0])):
+		i = int(k/len(puz))
+		j = int(k%len(puz[0]))
+		if puz[i][j] == "_":
+			blanks.append((i,j))
+	return blanks
 
 def solve(puz):
 	domain = ("b", "_")
@@ -253,6 +275,7 @@ countNodes=0
 def backtrack (puz, domain, notassigned):
 	if complete(puz):
 		return puz
+		
 	global countNodes
 	countNodes+=1
 	if countNodes%10000 == 0:
@@ -260,9 +283,10 @@ def backtrack (puz, domain, notassigned):
 
 	if len(notassigned)== 0:
 		return "back"
-	val = getFromHeuristic(puz,notassigned,'constrained')
-	i = int(val/len(puz))
-	j = int(val%len(puz))
+		
+	val = getFromHeuristic(puz,notassigned,'constraining')
+	i = val[0]
+	j = val[1]
 
 	for value in domain:
 		puz[i][j] = value
@@ -270,7 +294,6 @@ def backtrack (puz, domain, notassigned):
 			result = backtrack(puz, domain, notassigned)
 			if result != "back":
 				return result
-
 
 	notassigned.append(val)
 	return "back"
@@ -283,34 +306,37 @@ def clearSolution(puzzle):
 			if puzzle[i][j] == 'b':
 				puzzle[i][j] = '_'
 
+debug = "none"
 def main( argv = None ):
-    if (argv == None ):
-        argv = sys.argv[1:]
-    parser = argparse.ArgumentParser(description='create a modified Akari Puzzle')
-    parser.add_argument('--width', action='store', dest='width', type=int, default=8)
-    parser.add_argument('--height', action='store', dest='height', type=int, default=8)
-    parser.add_argument('--verbose', '-v', action='count', dest='verbose', default=0)
-    parser.add_argument('--count', '-c', action='store', dest='count', default=1)
-    parser.add_argument('--solution', '-s', action='store_true', dest='solution', default=False)
-    parser.add_argument('--bulbs', '-b', action='store', dest='bulbs', type=int, default=-1)
-    parser.add_argument('--walls', '-w', action='store', dest='walls', type=int, default=-1)
+	global debug
+	
+	if (argv == None):
+		argv = sys.argv[1:]
+	parser = argparse.ArgumentParser(description='create a modified Akari Puzzle')
+	parser.add_argument('--width', action='store', dest='width', type=int, default=8)
+	parser.add_argument('--height', action='store', dest='height', type=int, default=8)
+	parser.add_argument('--verbose', '-v', action='count', dest='verbose', default=0)
+	parser.add_argument('--count', '-c', action='store', dest='count', default=1)
+	parser.add_argument('--solution', '-s', action='store_true', dest='solution', default=False)
+	parser.add_argument('--bulbs', '-b', action='store', dest='bulbs', type=int, default=-1)
+	parser.add_argument('--walls', '-w', action='store', dest='walls', type=int, default=-1)
+	parser.add_argument('--debug', action='store', dest='debug', type=str, default="none")
+	
+	
+	args = parser.parse_args( argv )
+	if (args.verbose > 0 ):
+		print('AkariMaker {0}x{1} bulbs {2} walls {3}'.format(args.width, args.height, args.bulbs, args.walls ))
 
-    args = parser.parse_args( argv )
-    if (args.verbose > 0 ):
-        print('AkariMaker {0}x{1} bulbs {2} walls {3}'.format(args.width, args.height, args.bulbs, args.walls ))
+	puzzle = makePuzzles(args.count, args.width, args.height, args.bulbs, args.walls, args.solution )
 
-    puzzle = makePuzzles(args.count, args.width, args.height, args.bulbs, args.walls, args.solution )
+	clearSolution(puzzle)
 
-    clearSolution(puzzle)
+	debug = args.debug
+	print()
 
-    print()
+	printPuzzle(solve(puzzle))
 
-    printPuzzle(solve(puzzle))
-
-    print(countNodes)
-
-
-
+	print(countNodes)
 
 if __name__ == '__main__':
-    main()
+	main()
